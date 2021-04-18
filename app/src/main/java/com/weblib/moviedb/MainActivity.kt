029ba610d4871fc690976bc2043fdfb244ad04b3
Tuan DUNG TRAN <tdung@unowhy.com>
2021-04-19 00:31:40 +0200

    feature: implement all modules with koin, create the first log with the popular list  and fix the "Cannot access class 'retrofit2.Response'"

--- app/src/main/java/com/weblib/moviedb/MainActivity.kt
+++ app/src/main/java/com/weblib/moviedb/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.weblib.moviedb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.weblib.movieui.PopularViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val popularViewModel by viewModel<PopularViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popularViewModel.getAllPopulars()
+
+        popularViewModel.movieList.observe(this, {
+            Log.e("@@Populars", it.size.toString())
+            if (it.isNotEmpty() && it != null) {
+                Log.e("Populars", it.toString())
+            }
+        })
     }
 }
--- app/src/main/java/com/weblib/moviedb/MovieDBApplication.kt
+++ app/src/main/java/com/weblib/moviedb/MovieDBApplication.kt
@@ -1,4 +1,27 @@
 package com.weblib.moviedb
 
-class MovieDBApplication {
+import android.app.Application
+import com.weblib.movieui.apiModule
+import com.weblib.movieui.networkModule
+import com.weblib.movieui.providerModule
+import com.weblib.movieui.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MovieDBApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MovieDBApplication)
+            modules(
+                networkModule,
+                apiModule,
+                viewModelsModule,
+                providerModule,
+            )
+        }
+    }
 }
